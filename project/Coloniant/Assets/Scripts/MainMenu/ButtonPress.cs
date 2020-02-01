93a9a0a054ea8d92573cfc593d60d08919728910
aminkaveh <54646528+aminkaveh@users.noreply.github.com>
2020-02-01 17:00:27 -0500

    test

---
+++ project/Coloniant/Assets/Scripts/MainMenu/ButtonPress.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+
+public class ButtonPress : MonoBehaviour {
+
+	public bool isStart;
+
+	public bool isOption; 
+
+	public bool isQuit; 
+
+	public bool isOptionE; 
+
+	public GameObject start; 
+
+	public GameObject options; 
+
+	public void Start(){
+		start.SetActive(true);
+		options.SetActive(false); 
+	}
+
+	public void PlayPressed(){
+		SceneManager.LoadScene("Main",LoadSceneMode.Single);
+	}
+
+	public void OptionsPressed()
+	{
+		start.SetActive(false);
+		options.SetActive(true); 	
+	}
+
+	public void ExitPrssed()
+	{
+		Application.Quit();  
+	}
+
+	public void OptionsExotPressed()
+	{
+		start.SetActive(true);
+		options.SetActive(false);
+	}	
+
+} 
+
+
+
+
