40bfa1e70224cb51fcf0f52f4963a49e6f386ff8
henry5798 <46511450+henry5798@users.noreply.github.com>
2020-02-16 22:12:13 -0500

    misc

--- project/Coloniant/Assets/Scripts/Ants/Ant.cs
+++ project/Coloniant/Assets/Scripts/Ants/Ant.cs
@@ -40,12 +40,15 @@ public class Ant : MonoBehaviour {
 
     #region Inspector Fields
 
+    [SerializeField]
+    private SpriteRenderer antSpriteRenderer;
+    [SerializeField]
+    private int lifeSeconds;
 
     #endregion
 
     #region Run-Time fields
 
-    private int lifeSeconds;
     private AntState antState;
     private AntLevel antLevel;
     [HideInInspector]
@@ -90,6 +93,9 @@ public class Ant : MonoBehaviour {
                 break;
         }
 
+        antLevel = AntLevel.UNDER_GROUND;
+        ChangeView(AntManager.main.currentView);
+        antState = AntState.IDLE;
         StartCoroutine(waitToKillAnt());
 	}
 
@@ -103,7 +109,30 @@ public class Ant : MonoBehaviour {
 
     #region Public Methods
 
-    
+    // Switches between above ground a below ground
+    public void ChangeView(AntManager.SceneView view)
+    {
+        if (view == AntManager.SceneView.ABOVE_GROUND && antLevel == AntLevel.UNDER_GROUND)
+        {
+            antSpriteRenderer.enabled = false;
+        }
+        else if (view == AntManager.SceneView.ABOVE_GROUND && antLevel == AntLevel.ABOVE_GROUND)
+        {
+            antSpriteRenderer.enabled = true;
+        }
+        else if (view == AntManager.SceneView.UNDER_GROUND && antLevel == AntLevel.UNDER_GROUND)
+        {
+            antSpriteRenderer.enabled = true;
+        }
+        else if (view == AntManager.SceneView.UNDER_GROUND && antLevel == AntLevel.ABOVE_GROUND)
+        {
+            antSpriteRenderer.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Invalid combination of scene and ant views in ants!");
+        }
+    }
 
     #endregion
 
@@ -112,7 +141,7 @@ public class Ant : MonoBehaviour {
     // Kills the ant
     private void Die()
     {
-
+        Destroy(gameObject);
     }
 
     // Finds the next Waypoint in the path
---
+++ project/Coloniant/Assets/Scripts/UI/SpawnQueenButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnQueenButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnButton()
+    {
+        QueenAnt.main.AddAntToSpawn(QueenAnt.Ants.QUEEN, 1);
+    }
+}
