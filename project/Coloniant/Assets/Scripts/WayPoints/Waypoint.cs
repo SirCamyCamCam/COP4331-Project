ecd1ab3f018abea070c20395c69fbc5ac949892f
SirCamyCamCam <cameroncarstens@knights.ucf.edu>
2020-02-23 16:54:27 -0500

    Start of waypoints

---
+++ project/Coloniant/Assets/Scripts/WayPoints/Waypoint.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------
+// Coloniant - WaypointManager                          2/23/2020
+// Author(s): Cameron Carstens
+// Contact: cameroncarstens@knights.ucf.edu
+// --------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+    
+    #region Inspector Fields
+
+    [Header("Dependencies")]
+    [SerializeField]
+    private GameObject attachedGameObject;
+
+    [Header("Settings")]
+    [SerializeField]
+    private int maxAntsNum;
+
+    #endregion
+
+    #region Runt-Time Fields
+
+    private WaypointManager.WaypointType type;
+    private List<Waypoint> connectedWaypoints;
+    private WaypointManager.Level level;
+
+    #endregion
+
+    #region Monobehavior
+
+    private void Awake()
+    {
+        connectedWaypoints = new List<Waypoint>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    /*void Update () {
+		
+	}*/
+
+    #endregion
+
+    #region Public methods
+
+    // Sets the types for the waypoint and connected waypoints
+    public void SetUpWaypointTypes(WaypointManager.WaypointType typeToSet, WaypointManager.Level levelToSet, List<Waypoint> connected)
+    {
+        type = typeToSet;
+        level = levelToSet;
+        connectedWaypoints = connected;
+    }
+
+    // Adds a new waypoint
+    public void AddAConnectedWaypoint(Waypoint newWaypoint)
+    {
+        connectedWaypoints.Add(newWaypoint);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+}
