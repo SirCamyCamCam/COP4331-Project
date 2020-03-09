ceca216aaf5967deebc9834e648ac676d1a222d3
aminkaveh <54646528+aminkaveh@users.noreply.github.com>
2020-03-08 23:19:12 -0400

    added a script to farmSpawner

--- project/Coloniant/Assets/Scripts/TestScript/TestLineRenderer.cs
+++ project/Coloniant/Assets/Scripts/TestScript/TestLineRenderer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NUnit.Framework;
@@ -7,13 +8,14 @@ using UnityEngine.TestTools;
 public class TestLineRenderer {
 
     // Line Renderer Lets player see path of travel
-	/*[Test]
+	[Test]
     public void TestLineRendererTest()
     {
         List<Waypoint> waypointList = new List<Waypoint>();
         waypointList.Add(WaypointManager.main.SpawnWaypoint(WaypointManager.WaypointType.LEAF_SITE, WaypointManager.Level.ABOVE_GROUND, null, new Vector3(0, 0, 0)));
         WaypointManager.main.SpawnWaypoint(WaypointManager.WaypointType.ENTRANCE, WaypointManager.Level.ABOVE_GROUND, null, new Vector3(1, 1, 1));
         Assert.IsNotNull(GameObject.FindObjectOfType<LineRenderer>());
-    }*/
+    }
 
 }
+*/
