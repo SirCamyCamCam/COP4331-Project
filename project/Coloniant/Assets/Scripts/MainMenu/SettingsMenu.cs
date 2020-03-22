923bde90ba73674653ca854b089f7e3f8246e74e
aminkaveh <54646528+aminkaveh@users.noreply.github.com>
2020-03-21 21:28:24 -0400

    Settings menu complete
    the resolution will now be set to the standard of the computer
    the graphics are able to be changed from the settings menu
    the audio mixer now controlls all of the audio in the scene

--- project/Coloniant/Assets/Scripts/MainMenu/SettingsMenu.cs
+++ project/Coloniant/Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -2,12 +2,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.Audio; 
 
 public class SettingsMenu : MonoBehaviour {
 
+	Resolution[] resolutions; 
+	public Dropdown resolutionDropdown; 
+
+	public AudioMixer audioMixer; 
+	void Start()
+	{
+		resolutions = Screen.resolutions;
+		resolutionDropdown.ClearOptions(); 
+
+		List<string> options = new List<string>(); 
+
+		int currentResolutionIndex = 0; 
+
+		for (int i = 0; i < resolutions.Length; i++)
+		{
+			string option = resolutions[i].width + " x " + resolutions[i].height; 
+			options.Add(option);
+
+			if(resolutions[i].width == Screen.currentResolution.width && 
+			  resolutions[i].height == Screen.currentResolution.height)
+			{
+				currentResolutionIndex = i; 
+			} 
+		}
+		resolutionDropdown.AddOptions(options); 
+		resolutionDropdown.value = currentResolutionIndex; 
+		resolutionDropdown.RefreshShownValue(); 
+	}
+
+	public void SetResolution (int resolutionIndex)
+	{
+		Resolution resolution = resolutions[resolutionIndex]; 
+		Screen.SetResolution(resolution.width,resolution.height,Screen.fullScreen); 
+	}
 	public void SetFullScreen(bool isFull)
 	{
 		Screen.fullScreen = isFull; 
 	}
+
+	public void SetMasterVolume(float volume)
+	{
+		audioMixer.SetFloat("volume",volume); 
+	}
+
+	public void SetQuality (int qIndex)
+	{
+		QualitySettings.SetQualityLevel(qIndex);
+	}
+
+
+	
 	
 }
