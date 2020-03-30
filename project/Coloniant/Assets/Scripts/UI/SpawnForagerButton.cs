f2fd396aa725bd06701246a51ef58e6221a62082
henry5798 <46511450+henry5798@users.noreply.github.com>
2020-03-29 22:36:46 -0400

    Added delay and NumberOfAntsToSpawn decrementing

--- project/Coloniant/Assets/Scripts/UI/SpawnForagerButton.cs
+++ project/Coloniant/Assets/Scripts/UI/SpawnForagerButton.cs
@@ -2,18 +2,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SpawnForagerButton : MonoBehaviour {
 
+    public bool spawningAnts = false;
+    public int antsToSpawn = 0;
+
     public void SpawnAnt()
     {
+        
+ 
         var InputGameObject = GameObject.Find("Spawn Forager - InputField");
         var stringval = InputGameObject.GetComponent<TMPro.TMP_InputField>();
+        TextMeshProUGUI textMesh = stringval.GetComponent<TextMeshProUGUI>();
         var value = Convert.ToInt32(stringval.text);
-        if (value > 0){
-            for (int i = 0; i < value; i++){
-                AntManager.main.AddAntsToSpawn(Ant.AntType.FORAGER);
+        
+        if (value == 0){
+            antsToSpawn = 0;
+            spawningAnts = false;
+
+        }
+        if (spawningAnts){
+            return;
+        }
+        antsToSpawn = value;
+        spawningAnts = true;
+    }
+    
+    float timeSinceLastFrame = 0;
+    public void Update()
+    {
+        timeSinceLastFrame += Time.deltaTime;
+        if(spawningAnts && timeSinceLastFrame >= 1)
+        {
+            timeSinceLastFrame = 0;
+            var InputGameObject = GameObject.Find("Spawn Forager - InputField");
+            var stringval = InputGameObject.GetComponent<TMPro.TMP_InputField>();
+            
+            //decrease counter by 1
+            
+            if (antsToSpawn > 0){
+                antsToSpawn--;
+                AntManager.main.AddAntsToSpawn(Ant.AntType.TRASH_HANDLER);
+                stringval.text = antsToSpawn.ToString();
             }
-        };
+            else
+            {
+                spawningAnts = false;
+                stringval.text = null;
+            }
+        }
     }
 }
